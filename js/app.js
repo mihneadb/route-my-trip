1d2eef7f0e8790be8ee8a3c797e8df2c45ea5eaf
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2013-11-13 23:55:19 +0200

    foundation css

--- js/app.js
+++ js/app.js
@@ -9,7 +9,8 @@ function initialize() {
     mapOptions = {
         center: new google.maps.LatLng(-34.397, 150.644),
         zoom: 8,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        disableDefaultUI: true,
     };
     map = new google.maps.Map(document.getElementById("map-canvas"),
                                   mapOptions);
