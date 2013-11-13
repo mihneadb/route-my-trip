cfbefcb374fb9374bf63836a76ad32dafd792768
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2013-11-13 23:41:58 +0200

    clicking to add  markers

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
+var mapOptions = null;
+var map = null;
+var autocomplete = null;
+
+var input = document.getElementById("source");
+
+
 function initialize() {
-    var mapOptions = {
+    mapOptions = {
         center: new google.maps.LatLng(-34.397, 150.644),
         zoom: 8,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    var map = new google.maps.Map(document.getElementById("map-canvas"),
+    map = new google.maps.Map(document.getElementById("map-canvas"),
                                   mapOptions);
+    autocomplete = new google.maps.places.Autocomplete(input);
+
+    google.maps.event.addListener(map, "click", function(e) {
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(e.latLng.lat(), e.latLng.lng()),
+            map: map,
+            title: "Clicked here",
+        });
+    });
+
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
-var input = document.getElementById("source");
-var autocomplete = new google.maps.places.Autocomplete(input);
+
+input.onkeydown = function (e) {
+    if (e.which == 13) {
+        console.log("enter");
+    }
+};
 
