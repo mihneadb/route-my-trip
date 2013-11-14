6fc4a526cebcc3073af31025b083bbfde2c99a6c
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2013-11-15 00:04:11 +0200

    calc route + plot map with waypoints

--- js/app.js
+++ js/app.js
@@ -1,9 +1,37 @@
 var mapOptions = null;
 var map = null;
 var autocomplete = null;
+var directionsService = null;
+var directionsDisplay = null;
 
 var input = document.getElementById("source");
 
+var coordinates = [];
+
+function calcRoute() {
+    var waypoints = [];
+    for (var i = 1; i < coordinates.length - 1; i++) {
+        waypoints.push({
+            location: coordinates[i],
+            stopover: true,
+        });
+    }
+
+    var request = {
+        origin: coordinates[0],
+        destination: coordinates[coordinates.length - 1],
+        waypoints: waypoints,
+        travelMode: google.maps.TravelMode["DRIVING"],
+    }
+
+    directionsService.route(request, function(result, status) {
+        if (status === "ZERO_RESULTS") {
+            alert("Zero results.");
+            return;
+        }
+        directionsDisplay.setDirections(result);
+    });
+}
 
 function initialize() {
     mapOptions = {
@@ -17,12 +45,30 @@ function initialize() {
     autocomplete = new google.maps.places.Autocomplete(input);
 
     google.maps.event.addListener(map, "click", function(e) {
+        var pos = new google.maps.LatLng(e.latLng.lat(), e.latLng.lng());
         var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(e.latLng.lat(), e.latLng.lng()),
+            position: pos,
             map: map,
             title: "Clicked here",
         });
+        coordinates.push(pos);
+
+        if (coordinates.length >= 2) {
+            calcRoute();
+        }
     });
+
+    directionsService = new google.maps.DirectionsService();
+
+    var rendererOptions = {
+        draggable: false,
+        hideRouteList: true,
+        polylineOptions: {
+            strokeOpacity: 1,
+        },
+    };
+    directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
+    directionsDisplay.setMap(map);
 
 }
 
